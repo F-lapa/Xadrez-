8ddb2ecc1032da84433fd730a425bebcfc3998c7
F-lapa <fernandolapa1987@gmail.com>
2026-08-27 17:50:50 -0300

    Delete version.js

--- version.js
+++
@@ -1,8 +0,0 @@
-/* Atualize este arquivo SEMPRE que publicar uma versão nova. */
-window.XM_LATEST = {
-  version: "1.1.1",
-  apk: "xadrez-mestre.apk",
-  apkAbsolute: "",
-  message: "APK assinado com certificado de release (não é mais debug).",
-  force: false
-};
